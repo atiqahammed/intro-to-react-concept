fae3f451fce36f4c914aa771e6b8f34418902181
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-06-14 22:33:16 +0600

    data is being fetched from api

--- source/frontend/src/Screens/HomeScreen.js
+++ source/frontend/src/Screens/HomeScreen.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import data from '../data';
+import React, { useState, useEffect } from 'react';
+// import data from '../data';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function HomeScreen(props) {
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fatchData = async () => {
+			const { data } = await axios.get('/api/products');
+			setProducts(data);
+		};
+
+		fatchData();
+
+	}, []);
+
+
 	return (
 		<ul className="products">
             {
-            	data.products.map(product =>
+            	products.map(product =>
                       
-					<li className="product">
+					<li className="product" key={product._id}>
 						<Link to={ '/product/' + product._id }>
 							<img src={product.image} alt="test product" className="product-image" />
 						</Link>

4ec35102aa8d9df295bbac5c109bde58734cc7d9
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-07-26 18:25:21 +0600

    ADDED: added three necessary route

--- source/frontend/amazon-clone/src/App.js
+++ source/frontend/amazon-clone/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-          hello world
+      <div className='App'>
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>login</h1>
+          </Route>
+          <Route path='/'>
+            <h1>Home</h1>
+          </Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );

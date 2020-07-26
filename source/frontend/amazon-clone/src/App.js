2879e568cd1bd8b21ccd7492cc1460c32dd21d8e
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-07-26 21:19:28 +0600

    ADD: introduced navbar

--- source/frontend/amazon-clone/src/App.js
+++ source/frontend/amazon-clone/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './component/Home';
 import Header from './component/common/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className='App'>
         <Switch>
           <Route path='/checkout'>
@@ -14,10 +16,7 @@ function App() {
           <Route path='/login'>
             <h1>login</h1>
           </Route>
-          <Route path='/'>
-            <Header />
-            <h1>Home</h1>
-          </Route>
+          <Route path='/' component={Home}/>
         </Switch>
       </div>
     </BrowserRouter>
--- source/frontend/amazon-clone/src/component/common/Header.js
+++ source/frontend/amazon-clone/src/component/common/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-	return (
-		<nav className='header'>
-			<img
-				className='header_logo'
-				src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-				alt='Amazon Logo'
-			/>
 
+	const activeLinkStyle = {color : 'red'};
+
+	return (
+		<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+			<NavLink className='navbar-brand' to='/' exact>Application</NavLink>
+			<NavLink to='/topics' className='nav-link' activeLinkStyle={activeLinkStyle}>Topics</NavLink>
+				
 		</nav>
 	)
 }
